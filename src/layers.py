c8250b93acd0834e44b75d892a0752c96223256e
ghhernandes <ghh.hernandes@gmail.com>
2020-11-30 22:11:07 -0300

    functions updates

--- src/layers.py
+++ src/layers.py
@@ -16,34 +16,24 @@ class Layer:
 
 class Linear(Layer):
     def foward(self, x):
-        self.x = x
-        return (self.x @ self.weights.t()) + self.bias
+        return x @ self.w.t() + self.bias
 
-    def backward(self):
-        self.x.grad = self.weights.t() @ self.loss_grad
-        self.weights.grad = self.x @ self.loss_grad
-        self.bias.grad = self.loss_grad.sum(0)
-
-    def __call__(self, x):
-        return self.foward(x)
-
+    def __backward(self, out, x):
+        x.g = self.w.t() @ out.g 
+        self.w.g = x.t() @ out.g
+        self.bias.g = out.g.sum(0)
+    
 class ReLU():
-    def __init__(self, x):
-        self.x = x
-        self.foward()
-
     def foward(self, x):
-        self.out = self.x.clamp_min(0.)
-        return self.out
+        return self.x.clamp_min(0.)
 
-    def backward(self):
-        self.x.grad = (self.x > 0).float() * self.loss_grad
-
+    def __backward(self, out, x):
+        x.g = (x > 0).float() * out.g
 
 class MSE():
-    def __init__(self, x, target):
-        self.x = x
+    def foward(self, x, targets):
+        return (x.squeeze() - targets).pow(2).mean()
 
+    def __backward(self, out, x, targets):
+        x.g = 2. * (x.squeeze() - targets).unsqueeze(-1) / targets.shape[0]
 
-    def __call__(self, x):
-        return self.foward(x)
